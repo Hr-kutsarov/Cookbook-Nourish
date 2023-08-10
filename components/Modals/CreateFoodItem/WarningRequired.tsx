c52e8caaabb52ea8ae65b9fb4f9c02d677d4965c
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-10 10:21:05 +0300

    Component displaying warnings for forms with RHF.

---
+++ components/Modals/CreateFoodItem/WarningRequired.tsx
@@ -0,0 +1,12 @@
+import { IoWarningOutline } from 'react-icons/io5'
+
+interface WarningRequiredProps {
+    message?: string;
+}
+
+const WarningRequired: React.FC<WarningRequiredProps> = ({message}) => {
+    console.log(message)
+    return <span className='flex absolute items-center gap-2 -right-24 -top-2 px-2 py-1 bg-slate-50 shadow-md text-rose-600 rounded-md'><IoWarningOutline />{message}</span>
+}
+
+export default WarningRequired;
