e91ba69988ca5a67965a50238b8b4f08855a2b6f
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-16 22:41:19 +0300

    Updated Foods creation form.

--- components/Modals/CreateFoodItem/WarningRequired.tsx
+++ components/Modals/CreateFoodItem/WarningRequired.tsx
@@ -5,8 +5,8 @@ interface WarningRequiredProps {
 }
 
 const WarningRequired: React.FC<WarningRequiredProps> = ({message}) => {
-    console.log(message)
-    return <span className='flex absolute items-center gap-2 -right-24 -top-2 px-2 py-1 bg-slate-50 shadow-md text-rose-600 rounded-md'><IoWarningOutline />{message}</span>
+    // console.log(message)
+    return <span className='flex absolute items-center gap-2 right-10 -top-2 px-2 py-1 bg-slate-50 shadow-md text-rose-600 rounded-md'><IoWarningOutline />{message}</span>
 }
 
 export default WarningRequired;
