e5e2c42e62ece40693af581b031e03d897c25973
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-16 22:40:06 +0300

    Food zod schema

---
+++ models/Food.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export const FoodsObjFormSchema = z.object({
+  name: z.string({ required_error: "Required!" }).trim(),
+  calories: z.number().positive(),
+  proteins: z.number().positive(),
+  fats: z.number().positive(),
+  carbs: z.number().positive(),
+  seasons: z.array(
+        z.object({
+        value: z.string(),
+        label: z.string(),
+        })
+    ).nonempty({ message: "There are none selected." }),
+  taste: z.array(
+    z.object({
+    value: z.string(),
+    label: z.string(),
+    })).nonempty({ message: "Tasteless." }),
+  weight: z.array(
+    z.object({
+    value: z.string(),
+    label: z.string(),
+    })).nonempty({ message: "There are none selected." }),
+  volume: z.array(
+    z.object({
+    value: z.string(),
+    label: z.string(),
+    })).nonempty({ message: "There are none selected." }),
+  functionality: z.array(
+    z.object({
+    value: z.string(),
+    label: z.string(),
+    })).nonempty({ message: "Expands a different dimension." }),
+
+});
+
+export type FoodsFormSchema = z.infer<typeof FoodsObjFormSchema>;
