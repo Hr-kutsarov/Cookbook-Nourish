ccc5408009518de44d5b2acebb414ac948bbd80b
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-23 22:18:06 +0300

    Edited home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Header from "@/components/Header/Header";
 
-import { motion } from 'framer-motion'
-export const dynamic = 'force-dynamic'
+import { motion } from "framer-motion";
+export const dynamic = "force-dynamic";
 
-interface IndexProps {
-  children: React.ReactNode
-}
+// interface IndexProps {
+//   children: React.ReactNode;
+// }
 
-const Index: React.FC<IndexProps> = async ({children}) => {
-  const supabase = createServerComponentClient({ cookies })
+const Index = async () => {
+  const supabase = createServerComponentClient({ cookies });
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   return (
-      <section className='grid m-auto bg-slate-200 w-full h-auto items-center justify-center'>
-      <h1
-        className='text-5xl font-extralight'
-        >
-          Home
-        </h1>
-        {children}
+    <>
+      <Header />
+      <section className="mt-[7vh] flex bg-slate-200 w-full min-h-[93vh] items-center justify-center">
+        <h1 className="text-5xl font-extralight">Home</h1>
+        {/* {children} */}
       </section>
-  )
-}
+    </>
+  );
+};
 
 export default Index;
