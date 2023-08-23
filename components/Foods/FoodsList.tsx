657f97bb3b918296ab1bf3f7d486453b0789bd07
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-23 22:24:52 +0300

    Added Suspense to Food list

--- components/Foods/FoodsList.tsx
+++ components/Foods/FoodsList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, Suspense } from "react"
 
 import { Food } from "@/app/types/FoodTypes"
 import { motion, AnimatePresence } from "framer-motion"
@@ -9,17 +9,12 @@ import { useRouter } from "next/navigation"
 import FoodItem from "./FoodItem"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { twMerge } from "tailwind-merge"
-import useCreateFood from "@/hooks/createFoodModal"
-import HeadingFoodList from './HeadingFoodList'
-import useSideMenuHook from '@/hooks/sideMenu'
+import Loading from "@/app/loading"
 
 export default function FoodList({data}: {data: Food[]}) {
     // const [foods, setFoods] = useState(data)
     const router = useRouter()
     const supabase = createClientComponentClient();
-    const createFormHandler = useCreateFood()
-    const handlerSideMenu = useSideMenuHook();
-    
 
     useEffect(() => {
         // supabase will /watch this channel
@@ -34,30 +29,31 @@ export default function FoodList({data}: {data: Food[]}) {
         // on complete request the channel is removed
         return () => {
             supabase.removeChannel(channel)
-        }
+            }
         }, [])
 
     return (
-            <section className={twMerge('bg-slate-300 flex flex-col min-h-[96vh] p-2 gap-2 mt-[4rem] w-full max-w-[94%]', handlerSideMenu.isOpen && 'max-w-[75%]')}>
-            <HeadingFoodList />
-            {/* <AnimatePresence>
-                <motion.div 
-                initial={{opacity: 0, y: '150vh'}}
-                animate={{opacity: 1, y: '0'}}
-                exit={{opacity: 0, y: '150vh'}}
-                transition={{
-                    duration: 0.4,
-                    type: "spring",
-                    bounce: 0.15,
-                    delay: 0.1
-                }}
-                className={twMerge("bg-slate-50 h-auto w-full flex flex-col items-between p-1 rounded-md shadow-lg", createFormHandler.isOpen && '-z-40')}
-                >
-                    {data?.map((f) => <FoodItem key={f.id} item={f}/>)}
-            </motion.div> */}
-        
-            {data.map((f) => <FoodItem key={f.id} item={f}/>)}
-            {/* </AnimatePresence> */}
-        </section>
+        <Suspense fallback={<Loading />}>
+            <section className={twMerge('flex flex-col h-full mb-32 gap-2 lg:px-4 md:px-2 sm:px-1')}>
+            <AnimatePresence>
+                    <motion.div 
+                    initial={{opacity: 0, y: '30vh'}}
+                    animate={{opacity: 1, y: '1vh'}}
+                    exit={{opacity: 0, y: '60vh'}}
+                    transition={{
+                        duration: 0.6,
+                        type: "spring",
+                        bounce: 0.2,
+                        delay: 0.1
+                    }}
+                    className={twMerge("bg-slate-50 h-auto w-full flex flex-col items-between p-1 rounded-md shadow-lg")}
+                    >
+                        {data?.map((f) => <FoodItem key={f.id} item={f}/>)}
+                    </motion.div>
+            </AnimatePresence>
+            
+            </section>
+        </Suspense>
+
     )
 }
