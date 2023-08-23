bf8b7ccfa03632a384a883c0dea1361b2f3fc9ca
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-23 22:18:48 +0300

    CreateFoodModal goes to /add-food url, new comp

---
+++ app/add-food/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header/Header";
+import CreateFoodModal from "@/components/Modals/CreateFoodItem/CreateFoodItemModal";
+export default function AddFood () {
+    return (
+        <>
+        <Header />
+        <CreateFoodModal />
+        </>
+    )
+}
