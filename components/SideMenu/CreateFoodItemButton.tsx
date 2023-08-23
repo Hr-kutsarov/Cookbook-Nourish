13b58e4925687d22d85eac3581427e5dcaa936eb
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-23 22:29:30 +0300

    Changed from toggler to href

--- components/SideMenu/CreateFoodItemButton.tsx
+++ components/SideMenu/CreateFoodItemButton.tsx
@@ -1,15 +1,19 @@
+
+
 import useCreateFood from "@/hooks/createFoodModal";
 import { RxChevronRight } from 'react-icons/rx';
 import { motion } from 'framer-motion'
-
+import Link from 'next/link'
 const CreateFoodItemModalButton:React.FC = () => {
     const toggleFoodForm = useCreateFood();
 
     return (
-    <button 
-        className=' flex justify-between  w-full text-slate-500 hover:text-slate-700 text-md font-bold transition-all delay-75 duration-150 hover:translate-x-1 focus:outline-none'
-        onClick={toggleFoodForm.isOpen ? toggleFoodForm.onClose : toggleFoodForm.onOpen}>Add Food Item <RxChevronRight size={18} />
-    </button>
+        <Link 
+        className='flex justify-between  w-full text-slate-500 hover:text-slate-700 text-md font-bold transition-all delay-75 duration-150 hover:translate-x-1 focus:outline-none'
+        href='add-food'
+        >Add Food Item <RxChevronRight size={18} />
+        </Link>
+
     )
 }
 
