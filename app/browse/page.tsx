cc59a8d83750bfabfddb8bfcf20e72b37e66c546
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-23 22:19:55 +0300

    Modified /browse page.

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,11 +1,14 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { Suspense } from 'react';
-import Loading from './loading';
+import { Suspense, useEffect } from 'react';
+import Loading from '../loading';
+
 // export const dynamic = 'force-dynamic'
 
 import FoodList from "@/components/Foods/FoodsList";
+import Header from '@/components/Header/Header';
+import HeadingFoodList from '@/components/Foods/HeadingFoodList';
 
 export const revalidate = 30;
 
@@ -20,14 +23,24 @@ export default async function Browse() {
 		redirect('/login')
 	}
 
+
   return (
-      <Suspense fallback={<Loading />}>
-        <span className='flex flex-col w-full h-auto bg-slate-300'>
-            <section className='flex md:hidden bg-slate-50 min-h-[2rem] mt-[4.25rem] fixed w-auto p-2 rounded-b-md'>
-            <h1 className='text-slate-400 font-semibold text-sm'>Hello, {session.user.email}</h1>
-            </section>
-              <FoodList data={data ?? []}/>
-        </span>
-      </Suspense>
+    <section className='flex flex-col w-full'>
+    <Header />
+    <Suspense fallback={<Loading />}>
+      <span className="pt-[12vh]">
+        <section className='flex md:hidden min-h-[2rem] mt-[7vh] fixed w-auto p-2 rounded-b-md'>
+          <h1 className='text-slate-400 font-semibold text-sm'>Hello, {session.user.email}</h1>
+        </section>
+        <FoodList data={data ?? []}/>
+      </span>
+    </Suspense>
+    </section>
     )
+
+
+
+
+
+
 }
