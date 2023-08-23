006e423728f38c25ce21f57a8472c7379dff7fcd
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-23 22:22:21 +0300

    New component/page - Playground minigrame

---
+++ app/playground/page.tsx
@@ -0,0 +1,18 @@
+import Loading from "../loading";
+import { RxQuestionMark } from 'react-icons/rx'
+import ButtonLoader from "@/components/Loaders/ButtonLoader";
+
+
+export default function Playground() {
+    return (
+        <>
+        <Loading />
+        <span className="absolute top-[30%] left-[40%] text-green-200">
+            <ButtonLoader />
+        </span>
+        <span className="absolute top-[60%] left-[60%] text-green-400">
+            <ButtonLoader />
+        </span>
+        </>
+    )
+}
