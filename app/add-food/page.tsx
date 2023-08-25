be7a2138d6870739910c0dd83a8435eb2e16c532
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-25 18:19:03 +0300

    Missing import.

--- app/add-food/page.tsx
+++ app/add-food/page.tsx
@@ -1,6 +1,10 @@
 import Header from "@/components/Header/Header";
 import CreateFoodModal from "@/components/Modals/CreateFoodItem/CreateFoodItemModal";
+
+import useCreateFood from "@/hooks/createFoodModal";
+
 export default function AddFood () {
+
     return (
         <>
         <Header />
