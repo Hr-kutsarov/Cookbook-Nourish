7b82645ff6d612ecbfca8f8ee1cb82713d3594d7
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-26 20:28:33 +0300

    Added missing loading state for search button

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,10 +6,11 @@ import HeadingFoodList from '@/components/Foods/HeadingFoodList';
 import Header from '@/components/Header/Header';
 import Loading from '@/components/Loaders/loading'
 import FoodFilter from '@/components/Foods/FoodFilter'
+import Bookmarks from '@/components/Bookmarks/Bookmarks';
 
 // hooks
 import prioritySwitcher from '@/hooks/prioritySwitcher';
-import { Suspense, useEffect, useState } from 'react'
+import { Suspense, useEffect, useState, CSSProperties } from 'react'
 import FoodDataStore from '@/hooks/foodStore'
 
 // utils
@@ -39,7 +40,7 @@ export default function Search() {
   // -------> storing it in LocalStorage with Zustand
   const dataStorage = FoodDataStore();
 
-  const [loadingReset, setLoadingReset] = useState<boolean>(false)
+  const [loadingReset, setLoadingReset] = useState<boolean>(true)
 
   // this is unnecessary as I'm going to use React Hook Form and it has a built in state manager
   // const [searchParam, setSearchParam] = useState('')
@@ -72,12 +73,12 @@ export default function Search() {
   // ON SUBMIT SEARCH
   const onSubmit = async (formData: SearchDataType) => {
     const supabase = createClientComponentClient();
-
+    setLoadingReset(true)
     // PASSING EMPTY STRING WITH SEARCH RETURNS ALL DATA ???
 
     // IF THERE IS A SEARCH PARAM RETURN THE MATCHING QUERY
 
-  const { data, error } = await supabase.from('Foods').select().like('name', `%${formData.searchParam}%`)
+    const { data, error } = await supabase.from('Foods').select().like('name', `%${formData.searchParam}%`)
 
     if (error) {
       toast('There was an error');
@@ -113,16 +114,18 @@ export default function Search() {
 
   return (
     <Suspense fallback={<Loading />}>
-    <span className={twMerge('flex flex-row bg-slate-300 min-h-[100vh]', '')}>
+    <span className={twMerge('flex flex-col bg-slate-300 min-h-[100vh]', '')}>
       <Header />
-      
-        <section className={twMerge('flex flex-col h-auto w-full')}>
+      <span className='mt-[7vh]'>
+      <Bookmarks />
+      </span>
+      <section className={twMerge('flex flex-col h-auto w-full')}>
 
-        {/* SEARCH FORM */}
-        <>
+      {/* SEARCH FORM */}
+      <>
         <form 
         // max-h-[5vh] is the height of the search bar
-        className={twMerge(inputSearchStyles, 'mt-[7vh]', 'max-h-[6vh] lg:max-h-[5vh]')}
+        className={twMerge(inputSearchStyles, 'group relative', 'max-h-[6vh] lg:max-h-[5vh]')}
         onSubmit={handleSubmit(onSubmit)}
         >
           <label 
@@ -157,28 +160,41 @@ export default function Search() {
             className={twMerge('px-2 py-1 flex rounded-md focus:outline-none text-sm font-semibold text-slate-600 tracking-wide', 'max-w-[60%]')}
           />
 
+
+        {!loadingReset ?           
           <input 
             className={twMerge('px-3 py-1 flex rounded-md cursor-pointer text-sm font-semibold bg-slate-600 text-slate-50 ml-2 tracking-wide hover:bg-slate-400 hover:text-slate-50', 'max-w-[20%]')}
             type="submit" 
+          /> 
+          :           
+          <motion.div
+          className={twMerge('bg-slate-600')}
+          initial={{ opacity: 0}}
+            animate={{ opacity: 1}}
+            transition={{duration:1.8, delay: 0.2, repeat: Infinity}}
+        >
+          <BiSearch 
+            size={24} 
           />
-
+        </motion.div>
+        }
         </form>
 
         {/* More red flags than a national day in China with this DevTool thingy */}
         {/* <DevTool control={control} /> */}
-          <Toaster />
-        </>
+        <Toaster />
+      </>
 
-        {/* Heading */}
-        <HeadingFoodList />
+      {/* Heading */}
+      <HeadingFoodList />
 
-        {/* Filter buttons */}
-        <FoodFilter />
+      {/* Filter buttons */}
+      <FoodFilter />
 
-        {/* Search Results */}
-        <FoodList data={dataStorage.data}/>
-        
-      </section>
+      {/* Search Results */}
+      <FoodList data={dataStorage.data}/>
+      
+    </section>
     
     </span>
     </Suspense>
