36c0e5c8ad328d2c538590ce7568d6100d285307
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-26 20:29:25 +0300

    Added Bookmark nav link with counter.

--- components/Header/BookmarkedCounter.tsx
+++ components/Header/BookmarkedCounter.tsx
@@ -1,20 +1,30 @@
 'use client'
 
 import bookmarkFoodDataStore from '@/hooks/bookmarkFoodStorage'
-
-import { useEffect } from 'react';
+import NavItem from './NavItem';
+import { RxBookmark } from 'react-icons/rx'
+import { usePathname } from 'next/navigation'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function BookMarkedCounter() {
 
     const bookmarkHandler = bookmarkFoodDataStore();
-
-    useEffect(() => {
-        console.log(bookmarkHandler.data)
-    })
+    const pathname = usePathname()
+    const routes = useMemo(() => [
+        {
+            icon: RxBookmark,
+            label: 'Bookmarks',
+            active: pathname == '/bookmarks',
+            href: '/bookmarks'
+        },
+    ], [pathname])
     return (
-        <>
-        <p>asd</p>
-        {bookmarkHandler.data ? (<p>{bookmarkHandler.data.length}</p>) : null}
-        </>
+        <span className='flex group relative'>
+        <NavItem key='bookmarks' {...routes[0]} primary/>
+        <span className='absolute flex items-center justify-center left-4 rounded-full aspect-square p-[0.5rem]'>
+            {bookmarkHandler.data.length > 0 ? (<p className='text-sm text-slate-400 font-semibold group-hover:text-slate-600'>{bookmarkHandler.data.length}</p>) : null}
+        </span>
+        <span className='flex md:hidden'><RxBookmark /></span>
+        </span>
     )
 }
