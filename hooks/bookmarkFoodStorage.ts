54fdf64ea7de60611869899b80b1030270161d56
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-26 20:32:23 +0300

    Splitting the bookmark storage for calculations

--- hooks/bookmarkFoodStorage.ts
+++ hooks/bookmarkFoodStorage.ts
@@ -2,15 +2,35 @@ import { create } from 'zustand';
 
 import { Food } from '@/app/types/FoodTypes';
 
+export type CalcFoodType = {
+    id: string,
+    name: string,
+    calories: number,
+    proteins: number,
+    fats: number,
+    carbs: number,
+    price: number
+}
+
+
 interface BookmarkFoodDataStorePros {
     data: Food[];
     setData: (data: Food[]) => void;
+    delData: (data: Food[]) => void;
+    calcData: CalcFoodType[];
+    setCalcData: (data: CalcFoodType[]) => void;
+    delCalcData: (data: CalcFoodType[]) => void;
+
+
 }
 
 const bookmarkFoodDataStore = create<BookmarkFoodDataStorePros>((set) => ({
     data: [],
     setData: (data) => set({ data: data}),
     delData: () => set({ data: []}),
+    calcData: [],
+    setCalcData: (data: CalcFoodType[]) => set({ calcData: data}),
+    delCalcData: () => set({ data: []}),
     })
 )
 
