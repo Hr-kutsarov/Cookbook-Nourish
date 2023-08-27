965e5b24039e35a63baa3fb4210800f0137d1912
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-27 21:30:35 +0300

    Fixed Logout button gradient style.

--- components/Header/LogoutButton.tsx
+++ components/Header/LogoutButton.tsx
@@ -21,7 +21,7 @@ function LogoutButton({setUser}:{setUser: Dispatch<SetStateAction<string>>}) {
 
   return (
     <button
-      className="py-2 px-3 rounded-md text-sm text-white bg-gradient-to-b font-semibold tracking-wide from-green-800 to-teal-900 "
+      className="py-2 px-3 rounded-md text-sm text-white bg-gradient-to-br font-semibold tracking-wide from-green-600 to-teal-800 "
       onClick={signOut}
     >
       Logout
