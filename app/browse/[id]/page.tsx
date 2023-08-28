319fa9ef0ecf57324c9635ecce0873d0c44da6a0
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-08-28 11:47:23 +0300

    Details data throws a Hot toast errror on trigger.

--- app/browse/[id]/page.tsx
+++ app/browse/[id]/page.tsx
@@ -1,10 +1,12 @@
 // import supabase from "@/app/utils/supabase";
 import { notFound } from "next/navigation";
+
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import FoodDetails from '@/components/Foods/FoodDetails'
 import { Suspense } from "react";
 import Loading from "@/components/Loaders/loading";
+import { Toaster } from "react-hot-toast";
 
 // export async function generateStaticParams() {
 //     const { data: foods } = await supabase.from('Foods').select('id')
@@ -12,9 +14,9 @@ import Loading from "@/components/Loaders/loading";
 // }
 
 export default async function Post({ params: { id }}: { params: { id: string}}) {
-    const supabase = createServerComponentClient({ cookies })
-    const { data } = await supabase.from('Foods').select().match({ id }).single()
-    // const { data } = await supabase.from('Foods').select()
+    const supabase = createServerComponentClient({ cookies });
+    const { data } = await supabase.from('Foods').select().match({ id }).single();
+    const adminEmail = process.env.ADMIN_EMAIL!;
 
     // if(!data) {
     //     // return 404 page
@@ -22,10 +24,9 @@ export default async function Post({ params: { id }}: { params: { id: string}}) 
     // }
 
     return (
-        <Suspense fallback={<Loading />}>
-            <span className='bg-slate-300'>
-                {data ? <FoodDetails data={data} /> : <p>oppsie</p>}
-            </span>
-        </Suspense>
+        <span className='bg-slate-200 w-full h-full min-h-[100vh] flex items-center justify-center '>
+            <Toaster />
+            {data ? <FoodDetails data={data} admin={adminEmail} /> : <p>The item was deleted.</p>}
+        </span>
     )
 }
